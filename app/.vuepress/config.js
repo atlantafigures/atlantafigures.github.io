7b9277024d2f5a0013e9d480dab06e034f163354
Sabrina Ferguson <skheath@gatech.edu>
2019-06-18 19:43:11 -0400

    Set up starting point for custom vuepress theme

---
+++ app/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Atlanta Military Figure Society',
+  dest: 'dist',
+  head: [
+    ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'}]
+  ],
+  themeConfig: {
+    sidebar: [
+      { text: 'Home', link: '/' }
+    ]
+  }
+}
